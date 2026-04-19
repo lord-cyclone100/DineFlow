88c193d495ad27d56922eaad7b9cbe623c4bbd92
Deb <2480010@cognizant.com>
2026-04-19 18:49:40 +0530

    Added repositories

---
+++ backend/DineFlow/src/main/java/com/cyclone/dineflow/repository/PaymentRepository.java
@@ -0,0 +1,16 @@
+package com.cyclone.dineflow.repository;
+
+import com.cyclone.dineflow.entity.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * [Detailed description of the class's responsibility]
+ * * @author 2480010
+ *
+ * @version 1.0
+ * @since 19-04-2026
+ */
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, String> {
+}
